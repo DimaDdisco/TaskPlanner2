c6d1348c88e647bdf95205f2062ad558ebfd16d3
Дима <dimasiniykov@gmail.com>
2022-04-26 20:41:46 +0300

    Added Database Context (empty) and Home page with all the styles

---
+++ TaskPlanner2/Models/DataBase/TaskPlannerContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskPlanner2.Models.DataBase
+{
+    public class TaskPlannerContext : DbContext
+    {
+
+        public TaskPlannerContext(DbContextOptions<TaskPlannerContext> options) 
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
--- TaskPlanner2/Startup.cs
+++ TaskPlanner2/Startup.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -8,14 +10,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using TaskPlanner2.Models.DataBase;
+
 namespace TaskPlanner2
 {
     public class Startup
     {
+        private IConfiguration Configuration { get; set; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            string connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<TaskPlannerContext>(options => options.UseSqlServer(connection));
+
             services.AddControllersWithViews();
         }
 
